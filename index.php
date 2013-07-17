4a7709684d17e6cc024369cb91b94d7ba15755d8
Michael Berkovich <theiceberk@gmail.com>
2013-07-17 09:11:41 -0700

    reorganization

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require_once('library/Tr8n.php'); ?>
+<?php tr8n_init_client_sdk("http://localhost:3000", "29adc3257b6960703", "a5af33d9d691ce0a6"); ?>
 <?php include('library/Tr8n/Includes/Scripts.php'); ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
@@ -9,9 +11,9 @@
 
 <body>
 
-<? for($i=0; $i<100; $i++) { ?>
-    <? echo tr("You have {count||message}", null, array('count'=> $i)); ?><br>
-<? } ?>
+<?php for($i=0; $i<100; $i++) { ?>
+    <?php echo tr("You have {count||message}", null, array('count'=> $i)); ?><br>
+<?php } ?>
 
 </body>
 </html>
